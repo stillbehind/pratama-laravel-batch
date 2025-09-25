52e58d4bd046372f01887f2a04d72dc7d78b2e4f
andreybihan <andreybihan@gmail.com>
2025-09-25 12:59:02 +0700

    laravel 2

---
+++ tutoriallaravel2/resources/views/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.master')
+@section ('judul')
+Home
+@endsection
+
+@section('content')
+
+    <h1>SanberBook</h1>
+    <h3>Social Media Developer Santai Berkualitas</h3>
+    <p>Belajar dan Berbagi agar hidup ini semakin santai berkualitas</p>
+
+    <h2>Benefit Join SanberBook</h2>
+    <ul>
+      <li>Mendapatkan motivasi dan semangat dari sesama developer</li>
+      <li>Sharing knowledge dari para mastah Sanber</li>
+      <li>Dibuat oleh calon web developer terbaik</li>
+    </ul>
+
+    <h2>Cara Bergabung ke SanberBook</h2>
+    <ol>
+      <li>Mengunjungi Website ini</li>
+      <li>Mendaftar di <a href = "/register">Form Sign Up  </a> </p> </li>
+      <li>Selesai</li>
+    </ol>
+
+@endsection
+
+</html>
+
+        
+  
---
+++ tutoriallaravel2/resources/views/page/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+
+@section('judul')
+DASHBOARD
+
+@endsection
+
+@section('content')
+<h1>Selamat Datang {{$full_name}}</h1>
+
+  
+    <h3>Terima kasih telah hadir di Sanber Book</h3>
+    <h4>Sosial Media kita bersama</h4>
+    <p>Belajar dan Berbagi agar hidup ini semakin santai berkualitas</p>
+
+    @endsection
